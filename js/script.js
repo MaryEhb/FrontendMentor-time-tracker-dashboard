ec5709ba27afc951635c77696c9dc845de8c35d4
Mariem Ehab <45633841+MaryEhb@users.noreply.github.com>
2021-11-16 20:14:18 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,113 +1,26 @@
 /*Variables*/
-const data = [
-  {
-    title: "Work",
-    timeframes: {
-      daily: {
-        current: 5,
-        previous: 7,
-      },
-      weekly: {
-        current: 32,
-        previous: 36,
-      },
-      monthly: {
-        current: 103,
-        previous: 128,
-      },
-    },
-  },
-  {
-    title: "Play",
-    timeframes: {
-      daily: {
-        current: 1,
-        previous: 2,
-      },
-      weekly: {
-        current: 10,
-        previous: 8,
-      },
-      monthly: {
-        current: 23,
-        previous: 29,
-      },
-    },
-  },
-  {
-    title: "Study",
-    timeframes: {
-      daily: {
-        current: 0,
-        previous: 1,
-      },
-      weekly: {
-        current: 4,
-        previous: 7,
-      },
-      monthly: {
-        current: 13,
-        previous: 19,
-      },
-    },
-  },
-  {
-    title: "Exercise",
-    timeframes: {
-      daily: {
-        current: 1,
-        previous: 1,
-      },
-      weekly: {
-        current: 4,
-        previous: 5,
-      },
-      monthly: {
-        current: 11,
-        previous: 18,
-      },
-    },
-  },
-  {
-    title: "Social",
-    timeframes: {
-      daily: {
-        current: 1,
-        previous: 3,
-      },
-      weekly: {
-        current: 5,
-        previous: 10,
-      },
-      monthly: {
-        current: 21,
-        previous: 23,
-      },
-    },
-  },
-  {
-    title: "Self Care",
-    timeframes: {
-      daily: {
-        current: 0,
-        previous: 1,
-      },
-      weekly: {
-        current: 2,
-        previous: 2,
-      },
-      monthly: {
-        current: 7,
-        previous: 11,
-      },
-    },
-  },
-];
+let data = [];
 
 const boxs = document.querySelectorAll(".box");
 const options = document.querySelectorAll(".options button");
 
 /*Functions*/
+
+//Get data from json file
+const getData = (action) => {
+  fetch('./js/data.json')
+    .then(response => response.json())
+    .then(result => action(result))
+    .catch(st => {
+      console.log("getting data error: " + st)
+    })
+}
+
+//Function called after fetching data from json
+const storeAndPrevData = (result) => {
+  data.push(...result)
+  previewData("weekly")
+}
 
 //preview required data in its card
 const previewData = (option) => {
@@ -132,4 +45,4 @@ const optionSelected = (e) => {
 options.forEach((option) => option.addEventListener("click", optionSelected));
 
 /*calling function on page load*/
-previewData("weekly");
+getData(storeAndPrevData)
